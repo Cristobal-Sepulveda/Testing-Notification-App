8c1ceec5a31ad4c0814937633ed8be43361874f2
Cristobal-Sepulveda <sepulveda.cristobal.ignacio@gmail.com>
2021-01-08 00:45:06 -0300

    THIRD COMMIT: NotificationManager.sendNotification was Added

--- app/src/main/java/com/example/testingnotificationapp/receiver/AlarmReceiver.kt
+++ app/src/main/java/com/example/testingnotificationapp/receiver/AlarmReceiver.kt
@@ -16,19 +16,28 @@
 
 package com.example.testingnotificationapp
 
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.testingnotificationapp.R
+import com.example.testingnotificationapp.utils.sendNotification
 
 class AlarmReceiver: BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        // TODO: Step 1.10 [Optional] remove toast
-        Toast.makeText(context, context.getText(R.string.eggs_ready), Toast.LENGTH_SHORT).show()
+        //TODO: Step 3.0
+        val notificationManager = ContextCompat.getSystemService(
+                context,
+                NotificationManager::class.java
+        ) as NotificationManager
 
-        // TODO: Step 1.9 add call to sendNotification
+        notificationManager.sendNotification(
+                context.getText(R.string.eggs_ready).toString(),
+                context
+        )
 
     }
 
